987080c36bcfbc2e2356c7f15111e2346faa43a2
Carlo Rea <charlie.rea@gmail.com>
2026-05-01 13:30:12 +0200

    Fix3: IProdottoRepository sintassi, rimossi Page duplicati in WPF csproj

--- src/Core/SistemaF.Domain/Interfaces/IProdottoRepository.cs
+++ src/Core/SistemaF.Domain/Interfaces/IProdottoRepository.cs
@@ -54,5 +54,5 @@ public interface IProdottoRepository : IRepository<Prodotto>
 
     Task<bool> EsisteCodiceFarmacoAsync(CodiceProdotto codice, CancellationToken ct = default);
     Task<bool> EsisteCodiceEANAsync(CodiceEAN ean, CancellationToken ct = default);
-}
     Task<IReadOnlyList<Prodotto>> GetAllAsync(CancellationToken ct = default);
+}
